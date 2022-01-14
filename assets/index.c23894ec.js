d94b08aeef2d2f6eaff698246ec3871125e516da
Y80 <Y80@users.noreply.github.com>
2022-01-14 10:07:17 +0000

    deploy: d3653c0d490c2e2d0d4872349eac11f4dd40cc18

--- assets/index.c23894ec.js
+++
@@ -1,1 +0,0 @@
-import{L as a}from"./Layout.4847c319.js";import{d as t,c as u,l as e}from"./vendor.8ae065b7.js";var n=t({setup(){return()=>u(a,null,{default:()=>[u("p",null,[e("index \u9875\u9762")]),u("p",null,[e("TODO: \u5C55\u793A\u6E38\u5BA2\u53EF\u89C1\u7684\u4E66\u7B7E")])]})}});export{n as default};
---
+++ assets/login.d15266c4.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index.6a20136d.js";function r(e){return t.get("/authenticate",{params:{code:e}})}function n(){return t.get("/verify")}export{r as a,n as v};
