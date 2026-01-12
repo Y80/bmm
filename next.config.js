d900ea5d90addab718f49feaf9e50d8e751d7a05
Y80 <t.cpp@outlook.com>
2026-01-12 10:25:19 +0800

    refactor: 移除 edge 环境下的 webpack 配置
    因项目已迁移至 turbopack，不再需要 edge 环境下处理环境变量的特殊配置

--- next.config.js
+++ next.config.js
@@ -34,17 +34,6 @@ export default async function setup(phase) {
         hideDomPathAttr: true,
       }) : undefined
     },
-    webpack: (config, { webpack, nextRuntime }) => {
-      // TODO 使用 turbopack 后是否需要？
-      // https://github.com/vercel/next.js/discussions/39705
-      // fix: edge 环境无法加载环境变量
-      if (nextRuntime === 'edge') {
-        config.plugins.push(new webpack.DefinePlugin({
-          "process.env.AUTH_SECRET": JSON.stringify(process.env.AUTH_SECRET),
-        }))
-      }
-      return config
-    },
     experimental: {
       serverActions: { allowedOrigins: [domainHost] }
     },
